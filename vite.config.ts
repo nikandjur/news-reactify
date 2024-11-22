fb59d809797c861fc2e734446c744bfd34307cc8
andrey <examle@gmail.com>
2024-11-23 02:28:46 +0300

    tsx

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: [".js", ".jsx", ".ts", ".tsx"], // добавляем это
   },
   plugins: [react()],
 });
