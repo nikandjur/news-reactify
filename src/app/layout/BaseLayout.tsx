211d1ef1069fa9fee443ff81257f3c2dad1d9323
andrey <examle@gmail.com>
2024-11-23 00:48:15 +0300

    fix path

--- src/app/layout/BaseLayout.tsx
+++ src/app/layout/BaseLayout.tsx
@@ -1,10 +1,11 @@
 import { useTheme } from "@/app/providers/ThemeProvider";
-import { MainPage } from "@/pages/main/ui/MainPage";
+import { MainPage } from "../../pages/main/ui/MainPage";
 import { Header } from "@/widgets/header";
 
 function BaseLayout() {
   const { isDark } = useTheme();
-  console.log("Путь до компонента Main:", "@/pages/main");
+
+  console.log("Импортируемый путь:", "@/pages/main/ui/MainPage");
 
   return (
     <div className={`app ${isDark ? "dark" : "light"}`}>
