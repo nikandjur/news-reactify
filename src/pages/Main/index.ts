ecf21af3625f4ae4d36bf62cad7036cdc8fc0d39
andrey <examle@gmail.com>
2024-11-23 10:26:39 +0300

    Fix directory case sensitivity issue

--- src/pages/Main/index.ts
+++
@@ -1,6 +0,0 @@
-import { MainPage } from "@/pages/main/ui/MainPage";
-import { NewsByFilters } from "./ui/NewsByFilters/NewsByFilters";
-import { LatestNews } from "./ui/LatestNews/LatestNews";
-import { NewsFilters } from "./ui/NewsFilters/NewsFilters";
-
-export { LatestNews, NewsFilters, NewsByFilters, MainPage };
